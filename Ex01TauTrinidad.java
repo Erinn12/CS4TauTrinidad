aad418a5986cc21d5d9d0a7da51af8f471ab2ef7
enatrinidad <90197546+Erinn12@users.noreply.github.com>
2022-09-07 09:00:28 +0800

    Add files via upload

---
+++ Ex01TauTrinidad.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex01.tau.trinidad;
+
+/**
+ *
+ * @author TAU
+ */
+public class Ex01TauTrinidad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = "Erin";
+        String name = "Nina";
+        String name = "Sofia";
+        pisayYears
+        System.out.print(name);
+        // TODO code application logic here
+    }
+    
+}
