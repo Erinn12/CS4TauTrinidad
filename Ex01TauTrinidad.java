daccb6c97f24f545db7d182c869f9bc24860666f
enatrinidad <90197546+Erinn12@users.noreply.github.com>
2022-09-09 12:14:55 +0800

    Add files via upload

--- Ex01TauTrinidad.java
+++ Ex01TauTrinidad.java
@@ -15,11 +15,25 @@ public class Ex01TauTrinidad {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Person 1
         String name = "Erin";
-        String name = "Nina";
-        String name = "Sofia";
-        pisayYears
-        System.out.print(name);
+        int age = 15;
+        String favSubject = "Biology";
+        //Person 2
+        String name2 = "Jasmine";
+        int age2 = 23; 
+        String favSubject2 = "Math";
+        //Person 3
+        String name3 = "Isaac";
+        int age3 = 10;
+        String favSubject3 = "Math";
+        // Operations
+        //Difference
+        int ageDiff = age-age3;
+        //Comparison
+        boolean same = (favSubject2 == favSubject3);
+        //Output
+        System.out.printf(%s and %s have an age difference of %i years,name,name3,ageDiff);
         // TODO code application logic here
     }
     
