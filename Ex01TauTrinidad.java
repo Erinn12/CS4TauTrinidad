9cb5891b2e03f7b2eb76dc04d256a74e334cabd2
enatrinidad <90197546+Erinn12@users.noreply.github.com>
2022-09-15 23:16:49 +0800

    Add files via upload

--- Ex01TauTrinidad.java
+++ Ex01TauTrinidad.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ex01.tau.trinidad;
-
+package ex01.tau.trinidad; 
 /**
  *
  * @author TAU
@@ -19,22 +13,28 @@ public class Ex01TauTrinidad {
         String name = "Erin";
         int age = 15;
         String favSubject = "Biology";
+        
         //Person 2
         String name2 = "Jasmine";
         int age2 = 23; 
         String favSubject2 = "Math";
+        
         //Person 3
         String name3 = "Isaac";
         int age3 = 10;
         String favSubject3 = "Math";
+        
         // Operations
         //Difference
         int ageDiff = age-age3;
+        //Sum
+        int ageSum = age+age2+age3;
         //Comparison
-        boolean same = (favSubject2 == favSubject3);
+        boolean sameSubject = favSubject2 == favSubject3;
+        
         //Output
-        System.out.printf(%s and %s have an age difference of %i years,name,name3,ageDiff);
-        // TODO code application logic here
+        System.out.printf("%s and %s have an age difference of %i years.", name,name3,ageDiff);
+        System.out.printf("The ages of the three people combined is %i", ageSum);
+        System.out.printf("Isaac and Jasmine have the same favorite subject.: %b", sameSubject);
     }
-    
 }
